9a90c5eb1c9b53a4cdb2bf26f38c15dd7eb08f92
ToMMaaY <thuynh629@gmail.com>
2019-05-07 16:18:46 -0700

    linked dice and button to code

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Interface Builder Outlet
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // Interface Builder Action
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
