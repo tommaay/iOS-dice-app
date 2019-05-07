af74161808371796b1498d92e8845fa8368ce3dc
ToMMaaY <thuynh629@gmail.com>
2019-05-07 15:58:29 -0700

    completed the design

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Tommy Huynh on 5/7/19.
+//  Copyright © 2019 Tommy Huynh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
