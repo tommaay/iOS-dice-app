7d948fda61a4a2517a164083515f75a790b9a056
ToMMaaY <thuynh629@gmail.com>
2019-05-07 18:27:43 -0700

    added full functionality to the random dice app

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var randomDiceIndex1: Int = 0
-    var randomDiceIndex2: Int = 0
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6",]
+    
+    var randomDiceIndex1 = 0
+    var randomDiceIndex2 = 0
     
     // Interface Builder Outlet
     @IBOutlet weak var diceImageView1: UIImageView!
@@ -27,7 +29,9 @@ class ViewController: UIViewController {
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
         
-        print(randomDiceIndex1)
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
 }
